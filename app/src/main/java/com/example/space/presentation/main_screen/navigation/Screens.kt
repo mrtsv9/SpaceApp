74a818529948aae00282a711cf89b1f7d77c4e71
mrtsv <a.martsev85@gmail.com>
2022-04-01 02:32:25 +0300

    Setting Cicerone

---
+++ app/src/main/java/com/example/space/presentation/main_screen/navigation/Screens.kt
@@ -0,0 +1,10 @@
+package com.example.space.presentation.main_screen.navigation
+
+import com.example.space.presentation.main_screen.view.MainFragment
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+object Screens {
+
+    fun main() = FragmentScreen { MainFragment() }
+
+}
