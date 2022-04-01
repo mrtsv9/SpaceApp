a4edcc1904541f42ed95f199fdcfd59b36d93f61
MartsevPC <a.martsev85@gmail.com>
2022-04-01 17:38:33 +0300

    Setting cicerone

--- app/src/main/java/com/example/space/di/AppModule.kt
+++ app/src/main/java/com/example/space/di/AppModule.kt
@@ -2,6 +2,8 @@ package com.example.space.di
 
 import com.example.domain.service.ApiService
 import com.example.space.presentation.main_screen.interactor.MainInteractor
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.Router
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,5 +18,18 @@ object AppModule {
 
     @Provides
     fun provideMainInteractor(apiService: ApiService): MainInteractor = MainInteractor(apiService)
+
+    @Provides
+    @Singleton
+    fun provideCicerone(): Cicerone<Router> = Cicerone.create()
+
+    @Provides
+    @Singleton
+    fun provideRouter(cicerone: Cicerone<Router>) = cicerone.router
+
+    @Provides
+    @Singleton
+    fun getNavigationHolder(cicerone: Cicerone<Router>) = cicerone.getNavigatorHolder()
+
 
 }
