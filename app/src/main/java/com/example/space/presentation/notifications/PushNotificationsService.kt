c31a8503f6e1a43c22152afc9a9e763fb377270f
MartsevPC <a.martsev85@gmail.com>
2022-07-06 02:04:11 +0300

    notification from FCM done

--- app/src/main/java/com/example/space/presentation/notifications/PushNotificationsService.kt
+++ app/src/main/java/com/example/space/presentation/notifications/PushNotificationsService.kt
@@ -1,43 +1,64 @@
 package com.example.space.presentation.notifications
 
 import android.annotation.SuppressLint
-import android.util.Log
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.space.R
+import com.example.space.presentation.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class PushNotificationsService: FirebaseMessagingService() {
+const val channelId = "notification_channel"
+const val channelName = "com.example.space"
 
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-    }
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+@RequiresApi(Build.VERSION_CODES.M)
+class PushNotificationsService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // ...
+        if (remoteMessage.notification != null) {
+            val title = remoteMessage.notification!!.title
+            val message = remoteMessage.notification!!.body
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d("TAG", "From: ${remoteMessage.from}")
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d("TAG", "Message data payload: ${remoteMessage.data}")
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-
-            } else {
-                // Handle message within 10 seconds
-
-            }
+            sendNotification(title, message)
         }
+    }
 
-        // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
-            Log.d("TAG", "Message Notification Body: ${it.body}")
+    private fun sendNotification(title: String?, message: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSound(defaultSoundUri)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
         }
+        notificationManager.notify(0, notificationBuilder.build())
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 
 }
